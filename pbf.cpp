92cc8e7753dcc708a738fb31089592736cc833db
Naruki Yoshikawa <naruki.yoshikawa@gmail.com>
2021-12-08 10:42:05 -0500

    Fix indentation

--- pbf.cpp
+++ pbf.cpp
@@ -18,7 +18,7 @@ using VectorXd = Eigen::VectorXd;
 
 // clip function from https://stackoverflow.com/questions/9323903/most-efficient-elegant-way-to-clip-a-number
 float clip(float n, float lower, float upper) {
-  return std::max(lower, std::min(n, upper));
+    return std::max(lower, std::min(n, upper));
 }
 
 // cross product of two columns vectors from https://en.wikipedia.org/wiki/Cross_product
@@ -45,7 +45,7 @@ double W_poly6(Eigen::Ref<const VectorXd> r, double h) {
 VectorXd dW_spiky(Eigen::Ref<const VectorXd> r, double h) {
     double r_norm = r.norm();
     double c = 0.0;
-    
+
     if (r_norm <= 0.000001) { // stability issues with small values of r_nor
         c = 0.0;
     } else if (r_norm <= h) {
@@ -55,13 +55,13 @@ VectorXd dW_spiky(Eigen::Ref<const VectorXd> r, double h) {
 }
 
 std::tuple<RowMatrixXd, RowMatrixXd> step(Eigen::Ref<const RowMatrixXd> x,
-                                          Eigen::Ref<const RowMatrixXd> v0,
-                                          Eigen::Ref<const RowMatrixXd> force,
-                                          Eigen::Ref<const VectorXd> rho_init,
-                                          double dt,
-					  double h, 
-					  double boundary,
-					  double epsilon) {
+        Eigen::Ref<const RowMatrixXd> v0,
+        Eigen::Ref<const RowMatrixXd> force,
+        Eigen::Ref<const VectorXd> rho_init,
+        double dt,
+        double h, 
+        double boundary,
+        double epsilon) {
     RowMatrixXd v = v0 + force * dt;
     RowMatrixXd p = x + v * dt;
 
@@ -95,9 +95,9 @@ std::tuple<RowMatrixXd, RowMatrixXd> step(Eigen::Ref<const RowMatrixXd> x,
                 VectorXd r = p.row(i) - p.row(j);
                 rho_i += W_poly6(r, h);
             }
-	    rho[i] = rho_i;
+            rho[i] = rho_i;
             double C = rho[i] / rho0 - 1.0;
-    
+
             double Z = 0.0; // denominator for formula (11)
             // formula (8)
             for (int k : neighbor[i]) {
@@ -109,19 +109,19 @@ std::tuple<RowMatrixXd, RowMatrixXd> step(Eigen::Ref<const RowMatrixXd> x,
                             grad += dW_spiky(r, h);
                         }
                     }
-		    grad = grad / rho0;
+                    grad = grad / rho0;
                     Z += std::pow(grad.norm(), 2.0);
 
                 } else {
                     VectorXd r = p.row(i) - p.row(k);
                     VectorXd grad = -dW_spiky(r, h);
                     grad = grad / rho0;
-		    Z += std::pow(grad.norm(), 2.0);
+                    Z += std::pow(grad.norm(), 2.0);
                 }
             }
             lam[i] = - C / (Z + epsilon); 
-           
-	}
+
+        }
         // calculate delta p_i
         RowMatrixXd delta_p(N, 3);
         for (int i=0; i<N; i++) {
@@ -130,11 +130,11 @@ std::tuple<RowMatrixXd, RowMatrixXd> step(Eigen::Ref<const RowMatrixXd> x,
             for (int j : neighbor[i]) {
                 if (i != j) {
                     VectorXd r = p.row(i) - p.row(j);
-		    VectorXd corr(3); 
-		    corr << 0.1*h, 0.1*h, 0.1*h;
-		    double s_corr = -0.0001 * std::pow(W_poly6(r, h) / W_poly6(corr, h),4);
+                    VectorXd corr(3); 
+                    corr << 0.1*h, 0.1*h, 0.1*h;
+                    double s_corr = -0.0001 * std::pow(W_poly6(r, h) / W_poly6(corr, h),4);
                     //double s_corr = 0;
-		    delta_p.row(i) += (lam[i]+lam[j] + s_corr) * dW_spiky(r, h) / rho0;
+                    delta_p.row(i) += (lam[i]+lam[j] + s_corr) * dW_spiky(r, h) / rho0;
                 }
             }
         }
@@ -142,50 +142,50 @@ std::tuple<RowMatrixXd, RowMatrixXd> step(Eigen::Ref<const RowMatrixXd> x,
         for (int i=0; i<N; i++) {
             p.row(i) += delta_p.row(i);
             // collision detection: clip particle positions to boundary walls
-	    p(i, 0) = clip(p(i, 0), -1*boundary, boundary);
+            p(i, 0) = clip(p(i, 0), -1*boundary, boundary);
             p(i, 1) = clip(p(i, 1), -1*boundary, boundary);
-	    if (p(i, 2) < 0){ // TO DO: implement ceiling
-	        p(i,2) = 0;
-	    }
+            if (p(i, 2) < 0){ // TO DO: implement ceiling
+                p(i,2) = 0;
+            }
         }
     }
 
     for (int i=0; i<N; i++) {
         v.row(i) = (p.row(i) - x.row(i)) / dt;
     }
-    
+
     /*for (int i=0; i<N; i++) {
-	// VORTICITY
-	VectorXd omega_i = VectorXd::Zero(3);
-        for (int j : neighbor[i]) {
-	    if (i != j){
-	        VectorXd v_ij = v.row(j) - v.row(i);
-		VectorXd grad = dW_spiky(p.row(i)-p.row(j), h);
-		omega_i += cross_prod(v_ij, grad);
-	    }
-	}
+    // VORTICITY
+    VectorXd omega_i = VectorXd::Zero(3);
+    for (int j : neighbor[i]) {
+    if (i != j){
+    VectorXd v_ij = v.row(j) - v.row(i);
+    VectorXd grad = dW_spiky(p.row(i)-p.row(j), h);
+    omega_i += cross_prod(v_ij, grad);
+    }
+    }
 
-	VectorXd eta = VectorXd::Zero(3);
-	for (int j : neighbor[i]) {
-	    eta += dW_spiky(p.row(i)-p.row(j), h) * omega_i.norm();
-	}
-	// added small constant in case denominator = 0 (source: https://www.cs.ubc.ca/~rbridson/fluidsimulation/fluids_notes.pdf section 5.1)
-	VectorXd N_vort = eta / (eta.norm() + 10e-20);
+    VectorXd eta = VectorXd::Zero(3);
+    for (int j : neighbor[i]) {
+    eta += dW_spiky(p.row(i)-p.row(j), h) * omega_i.norm();
+    }
+    // added small constant in case denominator = 0 (source: https://www.cs.ubc.ca/~rbridson/fluidsimulation/fluids_notes.pdf section 5.1)
+    VectorXd N_vort = eta / (eta.norm() + 10e-20);
 
-	double epsilon_vort = 2/1000;
-	VectorXd f_vort = epsilon_vort * cross_prod(N_vort, omega_i); 
-	
+    double epsilon_vort = 2/1000;
+    VectorXd f_vort = epsilon_vort * cross_prod(N_vort, omega_i); 
 
-	// VISCOSITY 
-	VectorXd viscosity = VectorXd::Zero(3);
-	double c = 0.01;
-        for (int j : neighbor[i]) {
-	    viscosity +=(v.row(j) - v.row(i)) * W_poly6(p.row(i) - p.row(j), h);
-	}	
-	viscosity = c * viscosity;
 
-	v.row(i) += viscosity;
-        v.row(i) += dt * f_vort;
+    // VISCOSITY 
+    VectorXd viscosity = VectorXd::Zero(3);
+    double c = 0.01;
+    for (int j : neighbor[i]) {
+    viscosity +=(v.row(j) - v.row(i)) * W_poly6(p.row(i) - p.row(j), h);
+    }	
+    viscosity = c * viscosity;
+
+    v.row(i) += viscosity;
+    v.row(i) += dt * f_vort;
     }*/
 
     return std::make_tuple(p, v);
